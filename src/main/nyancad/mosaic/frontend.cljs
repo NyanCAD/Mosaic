f2fe4118c94ce7aeb32b208cb323dc23d3b43d70
Pepijn de Vos <pepijndevos@gmail.com>
2021-06-01 17:10:57 +0200

    draw mosfets

--- src/main/nyancad/mosaic/frontend.cljs
+++ src/main/nyancad/mosaic/frontend.cljs
@@ -2,6 +2,8 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]))
 
+(def grid-size 50)
+
 (defonce state (r/atom {::zoom [0 0 1000 500]}))
 
 (defn sign [n] (if (> n 0) 1 -1))
@@ -22,34 +24,112 @@
         tp (.matrixTransform p m)] ; local movement
     [(.-x tp) (.-y tp)]))
 
+(defn zoom-schematic [e]
+  (swap! state update-in [::zoom]
+    (fn [[x y w h]]
+      (let [dx (* (sign (.-deltaY e)) w 0.1)
+            dy (* (sign (.-deltaY e)) h 0.1)
+            [ex ey] (viewbox-coord e)
+            rx (/ (- ex x) w)
+            ry (/ (- ey y) h)]
+        [(- x (* dx rx))
+         (- y (* dy ry))
+         (+ w dx)
+         (+ h dy)]))))
+
+(defn drag-schematic [e]
+  (when (::dragging @state)
+    (swap! state update-in [::zoom]
+           (fn [[x y w h]]
+             (let [[dx dy] (viewbox-movement e)]
+               [(- x dx)
+                (- y dy)
+                w h])))))
+
+(defn tetris [pattern]
+  [:g
+  (for [[y s] (map-indexed #(vector (* grid-size %1) %2) pattern)
+        [x c] (map-indexed #(vector (* grid-size %1) %2) s)
+        :when (not= c " ")]
+    [:rect.tetris {:x x, :y y, :width grid-size, :height grid-size, :key [x y]}])])
+
+(defn ports [pattern]
+  [:g
+  (for [[y s] (map-indexed #(vector (* grid-size %1) %2) pattern)
+        [x c] (map-indexed #(vector (* grid-size %1) %2) s)
+        :when (not= c " ")]
+    [:circle.port {:cx (+ x (/ grid-size 2)), :cy (+ y (/ grid-size 2)), :r (/ grid-size 10), :key [x y]}])])
+
+(defn lines [arcs]
+  [:g
+   (for [arc arcs]
+     [:polyline {:points (map #(* % grid-size) (flatten arc))}])])
+
+(defn arrow [x y size]
+   [:polygon.arrow {:points 
+     (map #(* % grid-size)
+      [x y,
+       (+ x size) (+ y size)
+       (+ x size) (- y size)])}])
+
+(defn mosfet [x y & flags]
+  (let [t [" #"
+           "##"
+           " #"]
+        p [" #"
+           "##"
+           " #"]
+        arcs [
+    [[0.5 1.5]
+     [1 1.5]]
+    [[1 1]
+     [1 2]]
+    [[1.5 0.5]
+     [1.5 1]
+     [1.1 1]
+     [1.1 2]
+     [1.5 2]
+     [1.5 2.5]]
+    [[1.5 1.5]
+     [1.1 1.5]]]]
+  [:svg.device.mosfet {:x (* x grid-size),
+                       :y (* y grid-size),
+                       :width (* 3 grid-size),
+                       :height (* 3 grid-size),
+                       :class flags}
+   [:g {:width (* 3 grid-size),
+        :height (* 3 grid-size)}
+    [tetris t]
+    [lines arcs]
+    [arrow 1.2 1.5 0.15]
+    [ports p]]]))
+
 (defn schematic-canvas []
   [:svg {:xmlns "http://www.w3.org/2000/svg"
          :height "100%"
          :width "100%"
          :view-box (::zoom @state)
-         :on-wheel (fn [e] (swap! state update-in [::zoom]
-                                  (fn [[x y w h]]
-                                    (let [dx (* (sign (.-deltaY e)) w 0.1)
-                                          dy (* (sign (.-deltaY e)) h 0.1)
-                                          [ex ey] (viewbox-coord e)
-                                          rx (/ (- ex x) w)
-                                          ry (/ (- ey y) h)]
-                                      [(- x (* dx rx))
-                                       (- y (* dy ry))
-                                       (+ w dx)
-                                       (+ h dy)]))))
-         :on-mouse-down #(swap! state assoc ::dragging true)
-         :on-mouse-up #(swap! state assoc ::dragging false)
-         :on-mouse-move (fn [e]
-                          (when (::dragging @state)
-                            (swap! state update-in [::zoom]
-                                   (fn [[x y w h]]
-                                     (let [[dx dy] (viewbox-movement e)]
-                                       [(- x dx)
-                                        (- y dy)
-                                        w h])))))}
-
-   [:circle {:cx 50, :cy 50, :r 100}]])
+         :on-wheel zoom-schematic
+         :on-mouse-down #(when (= (.-button %) 1) (swap! state assoc ::dragging true))
+         :on-mouse-up #(when (= (.-button %) 1) (swap! state assoc ::dragging false))
+         :on-mouse-move drag-schematic}
+   [mosfet 0 0 :pmos :ccw]
+   [mosfet 2 1 :nmos]
+   [mosfet 1 3 :pmos :cw]
+   [mosfet -1 2 :nmos :mirror]
+   [mosfet 4 0 :pmos :ccw]
+   [mosfet 6 1 :nmos]
+   [mosfet 5 3 :pmos :cw]
+   [mosfet 3 2 :nmos :mirror]
+   [mosfet 0 4 :pmos :ccw]
+   [mosfet 2 5 :nmos]
+   [mosfet 1 7 :pmos :cw]
+   [mosfet -1 6 :nmos :mirror]
+   [mosfet 4 4 :pmos :ccw]
+   [mosfet 6 5 :nmos]
+   [mosfet 5 7 :pmos :cw]
+   [mosfet 3 6 :nmos :mirror]
+   ])
 
 (defn ^:dev/after-load init []
   (rd/render [schematic-canvas]
