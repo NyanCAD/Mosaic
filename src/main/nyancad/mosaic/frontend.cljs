a3ff731ace58c4f74bbe1e86f7d0488df6f19c08
Pepijn de Vos <pepijndevos@gmail.com>
2021-08-03 16:49:43 +0200

    rewrite wire handling

--- src/main/nyancad/mosaic/frontend.cljs
+++ src/main/nyancad/mosaic/frontend.cljs
@@ -7,6 +7,11 @@
 
 (defn sign [n] (if (> n 0) 1 -1))
 
+; like conj but nil defaults to set
+(defn sconj
+  ([s val] (conj (or s #{}) val))
+  ([s val & vals] (apply conj (or s #{}) val vals)))
+
 (defn update-keys
   ([m keys f] (into m (map #(vector % (f (get m %)))) keys))
   ([m keys f & args] (into m (map #(vector % (apply f (get m %) args))) keys)))
@@ -16,15 +21,11 @@
 
 (defonce state
   (r/atom
-   {
-    ::zoom [0 0 500 500],
-    ::theme :tetris
-    ::tool ::cursor
-    ::wires #{}
-    ::selected #{}
-    ;; ::dragging #{}
-    ::schematic {
-                 :mos1 {:x (+ 0  0), :y (+ 0 0), :transform (.rotate I 270), :cell :pmos}
+   {::ui {::zoom [0 0 500 500]
+          ::theme :tetris
+          ::tool ::cursor
+          ::selected #{}}
+    ::schematic {:mos1 {:x (+ 0  0), :y (+ 0 0), :transform (.rotate I 270), :cell :pmos}
                  :mos2 {:x (+ 2 0), :y (+ 1 0), :transform (.rotate I 0), :cell :nmos}
                  :mos3 {:x (+ 1 0), :y (+ 3 0), :transform (.rotate I 90), :cell :pmos}
                  :mos4 {:x (+ -1 0), :y (+ 2 0), :transform (.rotate I 180), :cell :nmos}
@@ -40,21 +41,28 @@
                  :mos6a {:x (+ 2 4), :y (+ 1 4), :transform (.rotate I 0), :cell :nmos}
                  :mos7a {:x (+ 1 4), :y (+ 3 4), :transform (.rotate I 90), :cell :pmos}
                  :mos8a {:x (+ -1 4), :y (+ 2 4), :transform (.rotate I 180), :cell :nmos}
-    }}))
+                 :wire1 {:x 0 :y 0 :transform I :cell :wire :wires #{[0 -1] [0 0]}}
+                 :wire2 {:x 0 :y 0 :transform I :cell :wire :wires #{[-1 -1] [-1 0]}}}}))
+
+(defonce schematic (r/cursor state [::schematic]))
+(defonce ui (r/cursor state [::ui]))
 
 (def mosfet-shape
   [" #"
    "##"
    " #"])
 
-(declare mosfet-sym)
+(declare mosfet-sym wire-sym wire-bg)
 ; should probably be in state eventually
 (def models {::bg {:pmos mosfet-shape
-                   :nmos mosfet-shape}
+                   :nmos mosfet-shape
+                   :wire #'wire-bg}
              ::conn {:pmos mosfet-shape
-                     :nmos mosfet-shape}
+                     :nmos mosfet-shape
+                     :wire []}
              ::sym {:pmos #'mosfet-sym
-                    :nmos #'mosfet-sym}})
+                    :nmos #'mosfet-sym
+                    :wire #'wire-sym}})
 
 (defn viewbox-coord [e]
   (let [^js el (.-currentTarget e)
@@ -73,7 +81,7 @@
     [(.-x tp) (.-y tp)]))
 
 (defn zoom-schematic [direction ex ey]
-  (swap! state update ::zoom
+  (swap! ui update ::zoom
     (fn [[x y w h]]
       (let [dx (* direction w 0.1)
             dy (* direction h 0.1)
@@ -88,7 +96,7 @@
   (apply zoom-schematic (sign (.-deltaY e)) (viewbox-coord e)))
 
 (defn button-zoom [dir]
-  (let [[x y w h] (::zoom @state)]
+  (let [[x y w h] (::zoom @ui)]
     (zoom-schematic dir (+ x (/ w 2)) (+ y (/ h 2)))))
 
 (defn port-locations [pattern v]
@@ -104,22 +112,27 @@
                  ny (+ (.-y p) mid)]]
          [(.round js/Math (+ gx nx)) (.round js/Math (+ gy ny))])))
 
-(defn get-model [layer model]
-  (let [m (get (get models layer) (:cell model))]
-    (assert m "no model")
-    m))
+(defn transform-selected [st tf]
+  (let [selected (get-in st [::ui ::selected])]
+    (update st ::schematic
+            update-keys selected
+            update :transform tf)))
 
-(defn update-ports [st]
-  (assoc st ::ports
-         (set (mapcat #(port-locations (get-model ::bg %) %)
-                      (vals (::schematic st))))))
+(defn delete-selected [st]
+  (let [selected (get-in st [::ui ::selected])]
+    (-> (apply update st ::schematic dissoc selected)
+        (assoc-in [::ui ::selected] #{}))))
 
-(defn remove-wire [e]
-  (let [coord (map #(.floor js/Math (/ % grid-size)) (viewbox-coord e))]
-    (swap! state update ::wires disj coord)))
+(defn remove-wire [st e]
+  (let [[x y] (map #(.floor js/Math (/ % grid-size)) (viewbox-coord e))
+        selected (first (get-in st [::ui ::selected]))
+        xo (get-in st [::schematic selected :x])
+        yo (get-in st [::schematic selected :y])
+        coord [(- x xo) (- y yo)]]
+    (update-in st [::schematic selected :wires] disj coord)))
 
 (defn drag-view [e]
-  (swap! state update ::zoom
+  (swap! ui update ::zoom
          (fn [[x y w h]]
            (let [[dx dy] (viewbox-movement e)]
              [(- x dx)
@@ -131,109 +144,128 @@
         [nx ny] (map #(/ % grid-size) (viewbox-coord e))]
     (swap! state
       (fn [st]
-        (update st ::schematic update-keys (::selected st)
-          (fn [device]
-            (-> device
-                (update :x #(+ (or % nx) dx))
-                (update :y #(+ (or % ny) dy)))))))))
+        (let [selected (get-in st [::ui ::selected])]
+          (update st ::schematic update-keys selected
+            (fn [device]
+              (-> device
+                  (update :x #(+ (or % nx) dx))
+                  (update :y #(+ (or % ny) dy))))))))))
 
 (defn drag-wire [e]
-  (let [coord (map #(.floor js/Math (/ % grid-size)) (viewbox-coord e))]
-    (when-not (contains? (::ports @state) coord)
-      (swap! state update ::wires conj coord))))
+  (let [[x y] (map #(.floor js/Math (/ % grid-size)) (viewbox-coord e))]
+    (swap! state
+           (fn [st]
+             (let [selected (first (get-in st [::ui ::selected]))
+                   xo (get-in st [::schematic selected :x])
+                   yo (get-in st [::schematic selected :y])
+                   coord [(- x xo) (- y yo)]]
+               (update-in st [::schematic selected :wires] sconj coord))))))
 
 (defn wire-drag [e]
-    (case (::dragging @state)
+    (case (::dragging @ui)
       ::view (drag-view e)
       ::wire (drag-wire e)
       nil))
 
 (defn cursor-drag [e]
-    (case (::dragging @state)
+    (case (::dragging @ui)
       ::view (drag-view e)
       ::wire (drag-wire e)
       ::device (drag-device e)
       nil))
 
 (defn eraser-drag [e]
-  (let [dragging (::dragging @state)]
+  (let [dragging (::dragging @ui)]
     (case dragging
       ::view (drag-view e)
-      ::wire (remove-wire e)
+      ::wire (swap! state remove-wire e)
       nil))) ;todo remove devices?
 
 (defn drag [e]
-  (case (::tool @state)
+  (case (::tool @ui)
     ::eraser (eraser-drag e)
     ::wire (wire-drag e)
     ::cursor (cursor-drag e)))
   
-(defn drag-start-wire [e]
-  (when (= (.-button e) 0)
-    (when (= (::tool @state) ::eraser) (remove-wire e))
-    (swap! state #(-> %
-                      update-ports
-                      (assoc ::dragging ::wire)))
-    (.stopPropagation e)))
-    
-(defn drag-start-device [k v e]
-  (.stopPropagation e)
-  (when-not (::dragging @state) ; skip the button press from a drag initiated from a toolbar button
-    (if (or (contains? (::selected @state) k) (.-shiftKey e))
-      (swap! state update ::selected conj k)
-      (swap! state assoc ::selected #{k}))
+(defn add-wire [st]
+  (let [name (keyword (gensym :wire))]
+    (-> st
+        (assoc-in [::schematic name] ; X/Y will be set on drag
+                  {:transform I, :cell :wire})
+        (update ::ui assoc
+                ::dragging ::wire
+                ::selected #{name}))))
+
+(defn drag-start [k type e]
+  ; skip the button press from a drag initiated from a toolbar button
+  (when-not (::dragging @ui) 
+    ; primary mouse click
     (when (= (.-button e) 0)
-      (if (= (::tool @state) ::eraser)
-        (swap! state #(-> %
-                          (update ::schematic dissoc k)
-                          (update ::selected disj k)))
-        (let [[x y] (map #(/ % grid-size) (viewbox-coord e))]
-          (swap! state assoc
-                 ::dragging ::device
-                 ::offsetx x
-                 ::offsety y))))))
+      (.stopPropagation e) ; prevent bg drag
+      (letfn [(update-selection [sel]
+                (if (or (contains? sel k)
+                        (.-shiftKey e))
+                  (sconj sel k)
+                  #{k}))
+              (drag-type [ui]
+                (assoc ui ::dragging
+                       (case (::tool ui)
+                         ::cursor ::device
+                         ::wire ::wire
+                         ::eraser type)))
+              (update-schematic [st]
+                (case [(get-in st [::ui ::tool]) type]
+                  [::eraser ::device] (delete-selected st)
+                  [::eraser ::wire] (remove-wire st e) ;; TODO
+                  [::wire ::device] (add-wire st)
+                  st))]
+        (swap! state (fn [st]
+                       (-> st
+                           (update-in [::ui ::selected] update-selection)
+                           (update ::ui drag-type)
+                           update-schematic)))))))
 
 (defn drag-start-background [e]
-  (swap! state assoc ::dragging
-         (cond
-           (= (.-button e) 1) ::view
-           (= ::wire (::tool @state)) ::wire)))
+  (swap! state
+         (fn [st]
+           (cond
+             (= (.-button e) 1) (assoc-in st [::ui ::dragging] ::view)
+             (= ::wire (get-in st [::ui ::tool])) (add-wire st)
+             :else st))))
   
 (defn drag-end [e]
   (letfn [(deselect [st e]
             (if (= (.-target e) (.-currentTarget e))
-              (assoc st ::selected #{})
+              (assoc-in st [::ui ::selected] #{})
               st))
           (end [st]
             (-> st
-                (assoc ::dragging nil)
+                (assoc-in [::ui ::dragging] nil)
                 (update ::schematic
-                           update-keys (::selected st)
+                           update-keys (get-in st [::ui ::selected])
                            update-keys [:x :y] #(.round js/Math %))
-                (deselect e)
-                update-ports))]
+                (deselect e)))]
     (swap! state end)))
 
-(defn tetris [x y k v]
+(defn tetris [x y _ _]
   [:rect.tetris {:x x, :y y
                  :width grid-size
                  :height grid-size
                  }])
 
-(defn port [x y k v]
+(defn port [x y _ _]
   [:circle.port {:cx (+ x (/ grid-size 2)),
                  :cy (+ y (/ grid-size 2)),
-                 :r (/ grid-size 10)
-                 :on-mouse-down drag-start-wire}])
+                 :r (/ grid-size 10)}])
 
 (defn device [size k v & elements]
   [:svg.device {:x (* (:x v) grid-size)
                 :y (* (:y v) grid-size)
                 :width (* size grid-size)
                 :height (* size grid-size)
-                :class [(:cell v) (when (contains? (::selected @state) k) :selected)]}
+                :class [(:cell v) (when (contains? (::selected @ui) k) :selected)]}
    [:g.position
-    {:on-mouse-down (fn [e] (drag-start-device k v e))}
+    {:on-mouse-down (fn [e] (drag-start k ::device e))}
     (into [:g.transform
            {:width (* size grid-size)
             :height (* size grid-size)
@@ -249,6 +281,15 @@
          ^{:key [x y]} [prim x y k v])]))
 
 
+(defn get-model [layer model]
+  (let [m (get (get models layer) (:cell model))]
+    (assert m "no model")
+    (cond
+      (fn? m) m
+      (= layer ::bg) (partial draw-pattern m tetris)
+      (= layer ::conn) (partial draw-pattern m port)
+      :else (throw (js/Error. "invalid model")))))
+
 (defn lines [arcs]
   [:<>
    (for [arc arcs]
@@ -262,21 +303,26 @@
        (+ x size) (- y size)])}])
 
 
-(defn wire-bg [x y]
-  [:rect.tetris.wire {:x (* x grid-size),
-                      :y (* y grid-size),
-                      :width grid-size,
-                      :height grid-size,
-                      :on-mouse-down drag-start-wire}])
+(defn wire-bg [name net]
+  (let [wires (:wires net)
+        xo (:x net)
+        yo (:y net)
+        wires (set (map (fn [[x y]] [(+ xo x) (+ yo y)]) wires))]
+    [:g {:on-mouse-down #(drag-start name ::wire %)}
+     (doall (for [[x y] wires]
+              [:rect.tetris.wire {:x (* x grid-size)
+                                  :y (* y grid-size)
+                                  :key [x y]
+                                  :width grid-size
+                                  :height grid-size
+                                  :class (when (contains? (::selected @ui) name) :selected)}]))]))
 
 (defn draw-wire [x y wires]
   (let [neigbours [[x (+ y 1)] [x (- y 1)] [(+ x 1) y] [(- x 1) y]]
-        ports (::ports @state)
-        is-wire (filter #(or (contains? wires %)
-                             (contains? ports %))
+        is-wire (filter #(contains? wires %)
                         neigbours)
         num (count is-wire)]
-  [:<>
+  [:<> 
    (when (= num 3) [:circle.wire {:cx (* (+ x 0.5) grid-size)
                                   :cy (* (+ y 0.5) grid-size)
                                   :r (/ grid-size 10)}])
@@ -287,6 +333,15 @@
                   :x2 (* (+ x2 0.5) grid-size)
                   :y2 (* (+ y2 0.5) grid-size)
                   :key [x y x2 y2]}])]))
+
+(defn wire-sym [name net]
+  (let [wires (:wires net)
+        xo (:x net)
+        yo (:y net)
+        wires (set (map (fn [[x y]] [(+ xo x) (+ yo y)]) wires))]
+    [:g {:on-mouse-down #(drag-start name ::wire %)}
+     (for [[x y] wires]
+          ^{:key [x y]} [draw-wire x y wires])]))
 
 (defn mosfet-sym [k v]
   (let [shape [[[0.5 1.5]
@@ -314,9 +369,10 @@
              (-> st
                  (assoc-in [::schematic name] ; X/Y will be set on drag
                            {:transform I, :cell cell})
-                 (assoc ::tool ::cursor
-                        ::dragging ::device
-                        ::selected #{name}))))))
+                 (update ::ui assoc
+                         ::tool ::cursor
+                         ::dragging ::device
+                         ::selected #{name}))))))
 
 ; icons
 (def zoom-in (r/adapt-react-class icons/ZoomIn))
@@ -338,26 +394,14 @@
                       :name key
                       :id name
                       :value name
-                      :checked (= name (get @state key))
-                      :on-change #(swap! state assoc key name)}]
+                      :checked (= name (get @ui key))
+                      :on-change #(swap! ui assoc key name)}]
              [:label {:for name :title disp} [icon]]]))])
 
-(defn transform-selected [tf]
-  (swap! state
-         (fn [st]
-           (-> st
-               (update ::schematic update-keys (::selected st) update :transform tf)
-               update-ports))))
-
-(defn delete-selected []
-  (swap! state (fn [st]
-                 (-> (apply update st ::schematic dissoc (::selected st))
-                     (assoc ::selected #{})))))
-
 (defn schematic-canvas []
-  [:div#app {:class (::theme @state)}
+  [:div#app {:class (::theme @ui)}
    [:div#menu
-      [:select {:on-change #(swap! state assoc ::theme (.. % -target -value))}
+      [:select {:on-change #(swap! ui assoc ::theme (.. % -target -value))}
      [:option {:value "tetris"} "Tetris"]
      [:option {:value "eyesore"} "Classic"]]
     [:span.sep]
@@ -367,19 +411,19 @@
       [eraser ::eraser "Eraser"]]]
     [:span.sep]
     [:a {:title "Rotate selected clockwise"
-         :on-click (fn [e] (transform-selected #(.rotate % -90)))}
+         :on-click (fn [_] (swap! state transform-selected #(.rotate % -90)))}
      [rotatecw]]
     [:a {:title "Rotate selected counter-clockwise"
-         :on-click (fn [e] (transform-selected #(.rotate % 90)))}
+         :on-click (fn [_] (swap! state transform-selected #(.rotate % 90)))}
      [rotateccw]]
     [:a {:title "Mirror selected horizontal"
-         :on-click (fn [e] (transform-selected #(.flipX %)))}
+         :on-click (fn [_] (swap! state transform-selected #(.flipX %)))}
      [mirror-horizontal]]
     [:a {:title "Mirror selected vertical"
-         :on-click (fn [e] (transform-selected #(.flipY %)))}
+         :on-click (fn [_] (swap! state transform-selected #(.flipY %)))}
      [mirror-vertical]]
     [:a {:title "Delect selected"
-         :on-click (fn [e] (delete-selected))}
+         :on-click (fn [_] (swap! state delete-selected))}
      [delete]]
     [:span.sep]
     [:a {:title "zoom in [scroll wheel/pinch]"
@@ -396,31 +440,26 @@
          :on-click #(add-device :pmos)}
      "P"]]
    [:div#sidebar
-    (doall (for [sel (::selected @state)]
+    (doall (for [sel (::selected @ui)]
              [:<> {:key sel}
-              [:h1 (:cell (get (::schematic @state) sel)) ": " sel]
+              [:h1 (get-in schematic [sel :cell]) ": " sel]
               [:form.properties
                [:label {:for "width"} "width"] [:input {:name "width" :type "number"}]
                [:label {:for "length"} "length"] [:input {:name "length" :type "number"}]]]))]
    [:svg#canvas {:xmlns "http://www.w3.org/2000/svg"
           :height "100%"
           :width "100%"
-          :view-box (::zoom @state)
+          :view-box (::zoom @ui)
           :on-wheel scroll-zoom
           :on-mouse-down drag-start-background
           :on-mouse-up drag-end
           :on-mouse-move drag}
-    (for [[x y] (::wires @state)]
-      ^{:key [x y]} [wire-bg x y])
-    (for [[k v] (::schematic @state)]
-      ^{:key k} [draw-pattern (get-model ::bg v) tetris k v])
-    (let [wires (::wires @state)]
-      (for [[x y] wires]
-        ^{:key [x y]} [draw-wire x y wires]))
-    (for [[k v] (::schematic @state)]
+    (for [[k v] @schematic]
+      ^{:key k} [(get-model ::bg v) k v])
+    (for [[k v] @schematic]
       ^{:key k} [(get-model ::sym v) k v])
-    (for [[k v] (::schematic @state)]
-      ^{:key k} [draw-pattern (get-model ::conn v) port k v])]])
+    (for [[k v] @schematic]
+      ^{:key k} [(get-model ::conn v) k v])]])
 
 (defn ^:dev/after-load init []
   (rd/render [schematic-canvas]
