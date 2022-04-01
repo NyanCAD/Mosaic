c4af199cc582c16b3d5e21233a009ed08cce46fb
Niharika <niharika@fsfe.org>
2022-04-01 15:10:49 +0200

    adding Mosaic files

--- src/main/nyancad/mosaic/libman.cljs
+++ src/main/nyancad/mosaic/libman.cljs
@@ -1,3 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Pepijn De Vos <pepijndevos@gmail.com>
+# SPDX-FileCopyrightText: 2022 NyanCAD Mosaic
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+
 (ns nyancad.mosaic.libman
   (:require [reagent.core :as r]
             [reagent.dom :as rd]
