3e08799ce5f6f65a29c6b110b2c1f2b27e2143f9
Pepijn de Vos <pepijndevos@gmail.com>
2025-09-10 16:20:36 +0200

    more efficient text index

--- src/main/nyancad/mosaic/couchdb.cljs
+++ src/main/nyancad/mosaic/couchdb.cljs
@@ -5,13 +5,23 @@
   "CouchDB view functions for model search"
   (:require [clojure.string :as str]))
 
+(defn generate-suffixes
+  "Generate search suffixes using reductions with reverse tokenization.
+  Elegant approach that builds suffixes from right-to-left."
+  [s]
+  (->> (re-seq #"[a-z]+|[0-9]+|_+" s)
+       reverse
+       (reductions #(str %2 %1))
+       (remove #(str/starts-with? % "_"))))
+
 (defn name-search-view
-  "CouchDB view function for name-based model search"
+  "CouchDB view function for name-based model search with word boundary optimization"
   [^js doc]
   (when (str/starts-with? (.-_id doc) "models:")
-    (let [name (str/lower-case (or (.-name doc) ""))]
-      (doseq [i (range (count name))]
-        (js/emit (subs name i)
+    (let [name (str/lower-case (or (.-name doc) ""))
+          suffixes (generate-suffixes name)]
+      (doseq [suffix suffixes]
+        (js/emit suffix
                  #js{:_id (.-_id doc)
                      :name (.-name doc)
                      :type (.-type doc)})))))
